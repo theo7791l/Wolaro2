53261b634749d41e028da415e39a2582539c50a1
theo7791l <c42439980@gmail.com>
2026-02-23 01:36:25 +0100

    fix(lint): add missing closing brace in import statement in ban.ts

--- src/modules/moderation/commands/ban.ts
+++ src/modules/moderation/commands/ban.ts
@@ -2,7 +2,7 @@ import {
   SlashCommandBuilder,
   ChatInputCommandInteraction,
   PermissionFlagsBits,
-  from 'discord.js';
+}   from 'discord.js';
 import { ICommand, ICommandContext } from '../../../types';
 import { EmbedStyles } from '../../../utils/embeds';
 
